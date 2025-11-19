d34cebc5cf8372ad6d5aa9009861e34a28812fbb
klm-blzs <klembalazs19@gmail.com>
2025-11-20 00:17:43 +0100

    Json input

---
+++ Hazi/Hazi_feladat/Hazi_feladat/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hazi_feladat
+{
+    public record Movie(
+    string Title,
+    string? Director = null,
+    int? Year = null,
+    string? Genre = null,
+    double? Rating = null
+    );
+}
---
+++ Hazi/Hazi_feladat/Hazi_feladat/Program.cs
@@ -0,0 +1,27 @@
+﻿using Hazi_feladat;
+using System.Text.Json;
+
+try
+{
+    string json = File.ReadAllText("movies.json");
+
+    List<Movie>? Movies = JsonSerializer.Deserialize<List<Movie>>(json);
+
+    if (Movies is not null)
+    {
+        Console.WriteLine("Film adatbázis betöltve:");
+        Console.WriteLine($"Összes film: {Movies.Count}");
+    }
+    else
+    {
+        Console.WriteLine("A JSON fájl üres vagy hibás formátumú.");
+    }
+
+   
+
+}
+catch (IOException ex)
+{
+    Console.WriteLine("Hiba történt a beolvasás közben:");
+    Console.WriteLine(ex.Message);
+}
